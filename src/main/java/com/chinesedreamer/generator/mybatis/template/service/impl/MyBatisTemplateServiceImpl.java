5390d180a46f68203e154d6d96588bad600f558f
Paris Tao <paristao1989@gmail.com>
2016-02-06 09:22:04 +0800

    test

---
+++ src/main/java/com/chinesedreamer/generator/mybatis/template/service/impl/MyBatisTemplateServiceImpl.java
@@ -0,0 +1,45 @@
+package com.chinesedreamer.generator.mybatis.template.service.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+
+import com.chinesedreamer.generator.common.constant.EncodingConstant;
+import com.chinesedreamer.generator.mybatis.template.entity.Dao;
+import com.chinesedreamer.generator.mybatis.template.entity.Mapper;
+import com.chinesedreamer.generator.mybatis.template.entity.Model;
+import com.chinesedreamer.generator.mybatis.template.service.MyBatisTemplateService;
+
+@Service
+public class MyBatisTemplateServiceImpl implements MyBatisTemplateService{
+	
+	@Autowired
+	private VelocityEngine ve;
+
+	@Override
+	public String getMapperTemplate(Mapper mapper) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("mapper", mapper);
+		return VelocityEngineUtils.mergeTemplateIntoString(
+				this.ve, 
+				"velocity/mybatis/mapper-template.vm", 
+				EncodingConstant.ENCODE_UTF_8, model);
+	}
+
+	@Override
+	public String getDaoTemplate(Dao dao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getModelTemplate(Model model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
