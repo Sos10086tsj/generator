84afc2652d90b4d3678545290df9a7380da62e3e
Paris Tao <paristao1989@gmail.com>
2016-02-14 16:29:01 +0800

    匹配db类型时，不比较精度

--- src/main/java/com/chinesedreamer/generator/mybatis/db/constant/TypeMapper.java
+++ src/main/java/com/chinesedreamer/generator/mybatis/db/constant/TypeMapper.java
@@ -3,8 +3,8 @@ package com.chinesedreamer.generator.mybatis.db.constant;
 public enum TypeMapper {
 	DEFAULT("JDBC","JAVA"),
 	CHAR("CHAR","String"),
-	VARCHAR("VARCHAR","JAVA"),
-	VARCHAR2("VARCHAR","JAVA"),
+	VARCHAR("VARCHAR","String"),
+	VARCHAR2("VARCHAR","String"),
 	NUMBER("NUMERIC","BigDecimal"),
 	NUMERIC("NUMERIC","BigDecimal"),
 	DECIMAL("DECIMAL","BigDecimal"),
@@ -32,7 +32,11 @@ public enum TypeMapper {
 	
 	public static TypeMapper GetType(String type) {
 		for (TypeMapper mapper : TypeMapper.values()) {
-			if (mapper.toString().equals(type)) {
+			int index = type.indexOf("(");
+			if (-1 != index) {
+				type = type.substring(0, index);
+			}
+			if (type.equals(mapper.toString())) {
 				return mapper;
 			}
 		}
